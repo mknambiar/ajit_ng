ba42a965ebd0b68c93aefedb8032546dc62a02ef
mknambiar <nambiar@ieee.org>
2025-06-05 16:07:39 +0530

    Use new structures to pass sitar port addressed.
    So we had to create ajit_ng.h header file for this

---
+++ ajit_ng.h
@@ -0,0 +1,29 @@
+struct dcache_out_struct {
+	void *d_context_port, *d_asi_port, *d_addr_port;
+	void *d_request_type_port, *d_byte_mask_port;
+	void *d_write_data_port;
+}
+
+typdef dcache_out_struct dcache_out;
+
+struct icache_out_struct {
+	void *i_context_port, *i_asi_port, *i_addr_port;
+	void *i_request_type_port, *i_byte_mask_port;
+}
+
+typdef icache_out_struct icache_out;
+
+struct dcache_in_struct {
+    void *d_mae_in_port;
+    void *d_read_data_port;    
+}
+
+typdef dcache_in_struct dcache_in;
+
+struct icache_in_struct {
+    void *i_mae_in_port;
+    void *i_inst_pair_port;
+	void *i_mmu_fsr_port;
+}
+
+typdef icache_in_struct icache_in;
